7055556a9391ebea5ce4d52235a979347ebf3d1a
lucas <scluckas@gmail.com>
2016-10-19 14:26:46 +0200

    Movie list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('test', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('https://api.themoviedb.org/3/movie/550?api_key=26fd39fae8600a9391d2cfb61e1dfca2').
-        then(function(response) {
-            $scope.movies = response.data;
-        });
+  $http.get('https://api.themoviedb.org/3/discover/movie?api_key=26fd39fae8600a9391d2cfb61e1dfca2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1').
+  then(function(response) {
+    $scope.movies = response.data;
+  });
 });
