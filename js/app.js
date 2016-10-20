0dd1fa5ce83e61c3c57c50d4d338ca69ff3de571
lucas <scluckas@gmail.com>
2016-10-20 09:50:45 +0200

    Search Company

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-angular.module('test', [])
-.controller('Hello', function($scope, $http) {
-  $http.get('https://api.themoviedb.org/3/discover/movie?api_key=26fd39fae8600a9391d2cfb61e1dfca2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1').
-  then(function(response) {
-    $scope.movies = response.data;
-  });
+angular.module('searchApp', [])
+.controller('searchCompany', function($scope, $http) {
+
+  $scope.movies = null;
+
+
+  $scope.searchCompany=function(){
+    $http.get('https://api.themoviedb.org/3/search/company?api_key=26fd39fae8600a9391d2cfb61e1dfca2&query=' + $scope.search).
+    then(function(response) {
+      console.log('https://api.themoviedb.org/3/search/company?api_key=26fd39fae8600a9391d2cfb61e1dfca2&query=warner' + $scope.search);
+      $scope.movies = response.data;
+    });
+  }
 });
