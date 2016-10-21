7baca74e0349380b73328bb0df6974a7aef24402
lucas <scluckas@gmail.com>
2016-10-21 15:24:53 +0200

    Material Design

--- js/app.js
+++ js/app.js
@@ -3,12 +3,15 @@ angular.module('searchApp', [])
 
   $scope.movies = null;
 
+  $http.get('https://api.themoviedb.org/3/movie/now_playing?api_key=26fd39fae8600a9391d2cfb61e1dfca2&language=en-US').
+  then(function(response) {
+    $scope.movies = response.data['results'];
+  });
 
   $scope.searchCompany=function(){
     $http.get('https://api.themoviedb.org/3/search/company?api_key=26fd39fae8600a9391d2cfb61e1dfca2&query=' + $scope.search).
     then(function(response) {
-      console.log('https://api.themoviedb.org/3/search/company?api_key=26fd39fae8600a9391d2cfb61e1dfca2&query=warner' + $scope.search);
-      $scope.movies = response.data;
+      $scope.companys = response.data['results'];
     });
   }
 });
